d2f2bd5258efd555eccffa6720a09d2e702d20df
Juan-Ramirez-Coder <124215732+Juan-Ramirez-Coder@users.noreply.github.com>
2023-05-20 12:46:13 -0500

    Update calculadora_de_descuentos.py

--- Ejercicios en Python/calculadora_de_descuentos.py
+++ Ejercicios en Python/calculadora_de_descuentos.py
@@ -1,21 +1,21 @@
 #Calculando de descuentos:
 
-num_producto = int(input("Digame cuantos productos son: ")) #preguntar la cantidad de descuentos
-option = True   #inicializa option
+num_producto = int(input("Digame cuantos productos son: ")) #preguntar la cantidad de productos
+option = True   #inicializando option con valor True
  
 while option != False:
     print("Tenemos los siguientes descuentos: 1|20%  2|50% ") #Escoger entre los descuentos disponibles
     descuento = int(input("Escoga entre uno numero: ")) #en caso no escoge entre los disponibles volver a prengutar
     precio_final = 0
 
-    if descuento == 1:
+    if descuento == 1: #descuento de 20%
         for i in range(num_producto): #itera cada producto y calcula los descuentos a cada uno  
             precio_original = float(input(f"Costo del producto {i+1}: ")) 
             precio_final += precio_original - (precio_original * 20 / 100)
         print(precio_final) #muestra el resultado final de los productos con el descuento aplicado
         option = False
         
-    elif descuento == 2:
+    elif descuento == 2: #descuento de 50%
         for i in range(num_producto):
             precio_original = float(input(f"Costo del producto {i+1}: "))
             precio_final += precio_original - (precio_original * 50 / 100)
